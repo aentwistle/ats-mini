410140e6ade51a57127aa3a08fb7fc4fbc8fc888
Marat Fayzullin <luarvique@gmail.com>
2025-04-17 11:26:44 -0400

    Adding RDS information.

--- ats-mini/Draw.cpp
+++ ats-mini/Draw.cpp
@@ -277,7 +277,6 @@ void drawScreen()
   // Draw S-meter
   drawSMeter(getStrength(rssi), METER_OFFSET_X, METER_OFFSET_Y);
 
-<<<<<<< HEAD
   // Indicate FM pilot detection
 #ifndef THEME_EDITOR
   if((currentMode==FM) && rx.getCurrentPilot())
@@ -287,20 +286,6 @@ void drawScreen()
   // Show station or channel name, if present
   if(*getStationName())
     drawStationName(getStationName(), RDS_OFFSET_X, RDS_OFFSET_Y);
-
-  // If additional station or program info present...
-  if(*getStationInfo() || *getProgramInfo())
-  {
-    // Draw station and program info
-    spr.fillRect(0, 130, 320, 30, TH.bg);
-    spr.drawString(getStationInfo(), RDS_OFFSET_X, RDS_OFFSET_Y + 20, 2);
-    spr.drawString(getProgramInfo(), RDS_OFFSET_X, RDS_OFFSET_Y + 35, 2);
-  }
-  else
-  {
-    // Draw tuner scale
-    drawScale(isSSB()? (currentFrequency + currentBFO/1000) : currentFrequency);
-  }
 
   // If there is station or program info...
   if(*getStationInfo() || *getProgramInfo())
